e498769000efe1a575a350ef992478ca59e874cd
Jan Altensen <info@stricted.net>
2020-05-18 12:28:10 +0200

    j7popelteskt: Simplify vulkan and opencl symlinks generation
    Change-Id: I25501e0ffcb86b8e21ee44c648b9f14bbb931b8b

--- Android.mk
+++ Android.mk
@@ -11,42 +11,15 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-LIBGLES_MALI_LIBRARY := /vendor/lib/egl/libGLES_mali.so
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.$(TARGET_BOARD_PLATFORM).so
 
-VULKAN_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/vulkan.exynos5.so
-$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating lib/hw/vulkan.exynos5.so symlink: $@"
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
 	@mkdir -p $(dir $@)
-	$(hide) ln -sf $(LIBGLES_MALI_LIBRARY) $@
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
 
-ALL_DEFAULT_INSTALLED_MODULES += \
-	$(VULKAN_SYMLINK)
-
-include $(CLEAR_VARS)
-
-LIBGLES_MALI_LIBRARY := /vendor/lib/egl/libGLES_mali.so
-
-LIBOPENCL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so
-$(LIBOPENCL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating lib/libOpenCL.so symlink: $@"
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf libOpenCL.so.1 $@
-
-LIBOPENCL1_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1
-$(LIBOPENCL1_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating lib/libOpenCL.so.1 symlink: $@"
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf libOpenCL.so.1.1 $@
-
-LIBOPENCL11_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1.1
-$(LIBOPENCL11_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating lib/libOpenCL.so.1.1 symlink: $@"
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf $(LIBGLES_MALI_LIBRARY) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += \
-	$(LIBOPENCL_SYMLINK) \
-	$(LIBOPENCL1_SYMLINK) \
-	$(LIBOPENCL11_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS)
 
 endif
