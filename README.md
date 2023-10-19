ab5304e55b82cf3a9f52208de39b96d643537ddf
roynatech2544 <whiteshell2544@naver.com>
2023-10-19 17:15:06 +0900

    j7popelteskt: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Samsung Galaxy Wide 2 (j7popelteskt)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_j7popelteskt.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from j7popelteskt device
+$(call inherit-product, device/samsung/j7popelteskt/device.mk)
+
+PRODUCT_DEVICE := j7popelteskt
+PRODUCT_NAME := lineage_j7popelteskt
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-J727S
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="j7popelteskt-user 9 PPR1.180610.011 J727SKSU5CUD2 release-keys"
+
+BUILD_FINGERPRINT := samsung/j7popelteskt/j7popelteskt:9/PPR1.180610.011/J727SKSU5CUD2:user/release-keys
