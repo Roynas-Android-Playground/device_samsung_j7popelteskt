a966c033cfadcfbe152564a01f44a06462576fd4
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-23 15:14:55 +0900

    j7popelteskt: Import rootdir from bootimage ramdisk

--- device.mk
+++ device.mk
@@ -5,3 +5,13 @@
 #
 
 LOCAL_PATH := device/samsung/j7popelteskt
+
+# Rootdir
+PRODUCT_PACKAGES += \
+   fstab.samsungexynos7870 \
+   init.baseband.rc \
+   init.samsungexynos7870.rc \
+   init.samsungexynos7870.usb.rc
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/rootdir/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
