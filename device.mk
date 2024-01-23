f9273c94d089b2894950f441ec3f741695b43828
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-23 22:44:01 +0900

    j7popelteskt: Provide libhidlbase/libhwbinder shims

--- device.mk
+++ device.mk
@@ -90,8 +90,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    libhidltransport.vendor \
-    libhwbinder.vendor
+    libhidltransport_vendor \
+    libhwbinder_vendor
 
 # Rootdir
 PRODUCT_PACKAGES += \
