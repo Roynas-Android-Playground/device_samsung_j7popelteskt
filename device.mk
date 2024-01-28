53b4e1eeaebbf16db320aab592e852a54fee19b3
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-28 11:43:50 +0900

    j7popelteskt: overlay: Initially pull from stock

--- device.mk
+++ device.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
     libOMX.Exynos.VP8.Encoder \
     libOMX.Exynos.WMV.Decoder
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
