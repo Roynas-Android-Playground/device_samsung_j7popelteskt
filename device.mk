eea0ce11eb0bf5a810505b1da9d39b8e0bb25bd1
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-30 15:15:50 +0900

    j7popelteskt: Use powerhal HIDL v1 for now

--- device.mk
+++ device.mk
@@ -190,10 +190,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.samsung-libperfmgr
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl
 
 # Protobuf
 PRODUCT_PACKAGES += \
